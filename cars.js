e4d59518ee92764dac7d13b835b808b88b9c5453
safin777 <safins@sitetuners.net>
2023-09-25 23:45:26 +0600

    readme added

---
+++ cars.js
@@ -0,0 +1,8 @@
+const ford = {
+    name: 'Ford',
+    year: 2015,
+    color: 'red',
+    model: 'Mustang'
+}
+
+module.exports = ford
---
+++ index.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+const car = require('./cars.js')
+const chalk = require('chalk')
+
+
+//console.log(chalk.blue()); //chalk template literal
+console.log(chalk.blue(process.env.NAME));
+
+console.log(chalk.blue(car.name, car.year, car.color, car.model));
+
