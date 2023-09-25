bc56ac0b1745c12c4cee7602c1a36e3e7941090d
safin777 <sayedislamsafin7770555@gmail.com>
2023-09-26 00:39:31 +0600

    updated few thing

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const chalk = require('chalk')
 
 //console.log(chalk.blue()); //chalk template literal
 console.log(chalk.blue(process.env.NAME));
-
 console.log(chalk.blue(car.name, car.year, car.color, car.model));
+
+
 
